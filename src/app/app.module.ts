50fdaeef1f74161cc8baf475f0ce5af03172f0b5
Armine <armineawtisian@gmail.com>
2021-07-14 23:03:31 +0400

    creating todo-card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { WarningComponent } from './warning/warning.component';
 import { SuccessComponent } from './success/success.component';
+import { TodoCardComponent } from './todo-card/todo-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WarningComponent,
     SuccessComponent,
+    TodoCardComponent,
 
   ],
   imports: [
